e71c7a342e7a51c8da21c9d5a0a44f055c01c2f5
Zhidong Zhu <zhuzhidong@gmail.com>
2022-01-08 16:29:29 +0800

    use gin-gonic to replace default http server

--- server.go
+++ server.go
@@ -1,55 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"strconv"
-	"sync"
+import "github.com/gin-gonic/gin"
 
-	"example.com/goserver/app"
-)
-
-var counter int
-var mutex = &sync.Mutex{}
-
-func appMeta(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		reqBody, err := ioutil.ReadAll(req.Body)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var meta app.Meta
-		err = meta.Parse(reqBody)
-		if err != nil {
-			log.Fatal("Failed to parse request yaml")
-		}
-		fmt.Fprintf(w, "%s\n%s", meta.Title, meta.Version)
-	}
+func newApp(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
 
-func incrementCounter(w http.ResponseWriter, r *http.Request) {
-	mutex.Lock()
-	counter++
-	fmt.Fprintf(w, strconv.Itoa(counter))
-	mutex.Unlock()
-}
-
-func queryParams(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.URL.Query() {
-		fmt.Printf("%s: %s\n", k, v)
-	}
+func listApps(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "apps",
+	})
 }
 
 func main() {
-	http.HandleFunc("/apps", appMeta)
-	http.HandleFunc("/increment", incrementCounter)
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
+	router := gin.Default()
+	v1 := router.Group("/v1")
+	{
+		v1.POST("/apps", newApp)
+		v1.GET("/apps", listApps)
+	}
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	router.Run(":8081")
 }
