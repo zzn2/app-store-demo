55a173d62f3173653844190a9dac9225d0c1bfc4
Zhidong Zhu <zhuzhidong@gmail.com>
2022-01-09 10:07:54 +0800

    add filter.Operator

---
+++ filter/operator.go
@@ -0,0 +1,44 @@
+package filter
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+// Operator defines the operation type in a filter rule.
+// Typically it is defined in a query string using LHS brackets like:
+//
+//    /employees?name[like]=Tom&age[gt]=25
+//
+// When there's no LHS brackets, it represents a precise match:
+//
+// .  /employees?name=Tommy
+//
+type Operator string
+
+const (
+	Equals      Operator = "=="
+	Like                 = "like"
+	LessThan             = "<"
+	GreaterThan          = ">"
+	Unknown              = "Unknown"
+)
+
+// Parse parses given text into Operation.
+// It returns the corresponding Operation object when parse succeed
+// and generates error when the operation is unrecognized.
+func Parse(text string) (Operator, error) {
+	switch strings.ToLower(text) {
+	case "":
+		return Equals, nil
+	case "like":
+		return Like, nil
+	case "lt":
+		return LessThan, nil
+	case "gt":
+		return GreaterThan, nil
+	default:
+		return Unknown, errors.New(fmt.Sprintf("Unrecognized operation type '%s'", text))
+	}
+}
---
+++ filter/operator_test.go
@@ -0,0 +1,34 @@
+package filter
+
+import "testing"
+
+func TestParse(t *testing.T) {
+	var tests = []struct {
+		input        string
+		want         Operator
+		errorMessage string
+	}{
+		{"", Equals, ""},
+		{"like", Like, ""},
+		{"Like", Like, ""},
+		{"LIKE", Like, ""},
+		{"lt", LessThan, ""},
+		{"gt", GreaterThan, ""},
+		{"other", Unknown, "Unrecognized operation type 'other'"},
+	}
+
+	for _, tt := range tests {
+		testname := tt.input
+		t.Run(testname, func(t *testing.T) {
+			op, err := Parse(tt.input)
+			if op != tt.want {
+				t.Errorf("Expect '%s' but got '%s'.", tt.want, op)
+			}
+			if err != nil {
+				if err.Error() != tt.errorMessage {
+					t.Errorf("Expect error message '%s' but got '%s'.", tt.errorMessage, err.Error())
+				}
+			}
+		})
+	}
+}
