578c9a8a84f86d9072d2a9573c4bf68a742321ce
Zhidong Zhu <zhuzhidong@gmail.com>
2022-01-09 12:58:00 +0800

    update implementation of filter.Rule

--- filter/rule.go
+++ filter/rule.go
@@ -1,11 +1,83 @@
 package filter
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"regexp"
 
+	"example.com/goserver/filter/op"
+)
+
+// Rule describes a filter rule.
+// Typically it is defined in a query string using LHS brackets like:
+//
+//    name[like]=Tom -> To find name like "Tom"
+//    name=Tome      -> To find name exactly is "Tom"
+//    age[gt]=25     -> To find age > 25
+//
 type Rule struct {
 	FieldName string
-	Op        Operator
+	Op        op.Operator
 	Value     interface{}
+}
+
+var (
+	regexForPlainParam          = regexp.MustCompile(`^[a-zA-Z0-9.]+$`)
+	regexForParamWithLhsBracket = regexp.MustCompile(`^(?P<name>[a-zA-Z0-9]+)\[(?P<op>[a-zA-Z]*)\]$`)
+)
+
+// getNameAndOp parses a given text and separate them into name and operator.
+// The text is expected to be in following format:
+//
+//     param        returns param, op.Equals
+//     param[like]  returns param, op.Like
+//     param[gt]    returns param, op.GreaterThan
+//
+// For unrecognized operator names or illegal input formats, return error.
+func getNameAndOp(text string) (name string, operator op.Operator, err error) {
+	bytes := []byte(text)
+	if regexForPlainParam.Match(bytes) {
+		return text, op.Equals, nil
+	} else if regexForParamWithLhsBracket.Match(bytes) {
+		// Given "param[like]", returns:
+		//   [0]: param[like]
+		//   [1]: param
+		//   [2]: like
+		// So, index 1 will be the param and index 2 will be the operator
+		match := regexForParamWithLhsBracket.FindStringSubmatch(text)
+		if operator, err = op.Parse(match[2]); err != nil {
+			return match[1], operator, err
+		} else {
+			return match[1], operator, nil
+		}
+	} else {
+		return "", op.Unknown, errors.New(fmt.Sprintf("Malformed input key format: '%s'", text))
+	}
+}
+
+// New creates a new instance of Rule.
+// The nameAndOp shows the parameter name to be validated and with a operational operator in LHS bracket.
+// value refers the value attached to the check rule.
+// e.g.
+//
+//    name[like], Tom  -> name likes "Tom"
+//    name=Tome        -> name is exactly "Tom"
+//    age[gt]=25       -> age > 25
+//
+func New(nameAndOp string, value string) (*Rule, error) {
+	name, operator, error := getNameAndOp(nameAndOp)
+	if error != nil {
+		return nil, error
+	}
+
+	// TODO: Assert the value should represent a number when the op is LessThan or GreaterThan.
+	//       I'm not going to implement the details for this demo project since it's not used in the cases.
+
+	return &Rule{
+		FieldName: name,
+		Op:        operator,
+		Value:     value,
+	}, nil
 }
 
 func (r Rule) String() string {
--- filter/rule_test.go
+++ filter/rule_test.go
@@ -2,37 +2,128 @@ package filter
 
 import (
 	"fmt"
+	"strings"
 	"testing"
+
+	"example.com/goserver/filter/op"
 )
 
-func TestString(t *testing.T) {
+func TestGetNameAndOp(t *testing.T) {
 	var tests = []struct {
-		testName string
-		input    Rule
-		want     string
+		testName       string
+		input          string
+		expectedName   string
+		expectedOp     op.Operator
+		expectedErrMsg string
 	}{
 		{
-			"name=Tom",
-			Rule{FieldName: "name", Op: Equals, Value: "Tom"},
-			"Rule: name == Tom",
+			"Normal param name without brackets",
+			"param",
+			"param",
+			op.Equals,
+			"",
 		},
 		{
-			"name[like]=Tom",
-			Rule{FieldName: "name", Op: Like, Value: "Tom"},
-			"Rule: name like Tom",
+			"Param name with empty LHS bracket",
+			"param[]",
+			"param",
+			op.Equals,
+			"",
 		},
 		{
-			"age[lt]=20",
-			Rule{FieldName: "age", Op: LessThan, Value: "20"},
-			"Rule: age < 20",
+			"Param name with LHS brackets (op.Like)",
+			"param[like]",
+			"param",
+			op.Like,
+			"",
+		},
+		{
+			"Param name with LHS brackets (op.LessThan)",
+			"param[lt]",
+			"param",
+			op.LessThan,
+			"",
+		},
+		{
+			"Param name with LHS brackets (invalid op)",
+			"param[dummy]",
+			"param",
+			op.Unknown,
+			"Unrecognized operator type 'dummy'",
+		},
+		{
+			"Illegal input text",
+			"param[",
+			"",
+			op.Unknown,
+			"Malformed input key format: 'param['",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.testName, func(t *testing.T) {
-			str := fmt.Sprintf("%s", tt.input)
-			if str != tt.want {
-				t.Errorf("Expect '%s' but got '%s'.", tt.want, str)
+			name, op, err := getNameAndOp(tt.input)
+			if name != tt.expectedName {
+				t.Errorf("Expect name to be '%s' but got '%s'.", tt.expectedName, name)
+			}
+			if op != tt.expectedOp {
+				t.Errorf("Expect operator to be '%s' but got '%s'.", tt.expectedOp, op)
+			}
+			if err != nil {
+				if err.Error() != tt.expectedErrMsg {
+					t.Errorf("Expect err to be '%s' but got '%s'.", tt.expectedErrMsg, err.Error())
+				}
+			}
+		})
+	}
+}
+
+func TestNew(t *testing.T) {
+	var tests = []struct {
+		input           string
+		expectedRuleStr string
+		expectedErrMsg  string
+	}{
+		{
+			"name=Tom",
+			"Rule: name == Tom",
+			"",
+		},
+		{
+			"name[like]=Tom",
+			"Rule: name like Tom",
+			"",
+		},
+		{
+			"age[lt]=20",
+			"Rule: age < 20",
+			"",
+		},
+		{
+			"age[dummy]=20",
+			"<nil>",
+			"Unrecognized operator type 'dummy'",
+		},
+		{
+			"illegal/keyformat=1",
+			"<nil>",
+			"Malformed input key format: 'illegal/keyformat'",
+		},
+	}
+
+	for _, tt := range tests {
+		testName := tt.input
+		t.Run(testName, func(t *testing.T) {
+			keyAndValue := strings.Split(tt.input, "=")
+			rule, err := New(keyAndValue[0], keyAndValue[1])
+			str := fmt.Sprintf("%s", rule)
+			if str != tt.expectedRuleStr {
+				t.Errorf("Expect '%s' but got '%s'.", tt.expectedRuleStr, str)
+			}
+			if err != nil {
+				if err.Error() != tt.expectedErrMsg {
+					t.Errorf("Expect err to be '%s' but got '%s'.", tt.expectedErrMsg, err.Error())
+				}
 			}
 		})
 	}
