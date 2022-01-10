fb6aaeb6b63edad6a50603e5845bb40a41e27372
Zhidong Zhu <zhuzhidong@gmail.com>
2022-01-11 00:06:49 +0800

    add get app by title

--- src/app/store.go
+++ src/app/store.go
@@ -15,6 +15,20 @@ func (s *Store) Add(app Meta) {
 	s.apps = append(s.apps, app)
 }
 
+func (s *Store) GetByTitle(title string) *Meta {
+	return s.lastOrNil(func(app Meta) bool {
+		return app.Title == title
+	})
+}
+
+func (s *Store) GetByTitleAndVersion(title string, version string) *Meta {
+	// If multiple found, return the last one, which is likely to be the latest version.
+	// TODO: Should add version comparation logic here and only return the lastest version.
+	return s.lastOrNil(func(app Meta) bool {
+		return app.Title == title && app.Version == version
+	})
+}
+
 func (s *Store) List(ruleSet filter.RuleSet) ([]Meta, error) {
 	result := make([]Meta, 0)
 	for _, app := range s.apps {
@@ -29,4 +43,36 @@ func (s *Store) List(ruleSet filter.RuleSet) ([]Meta, error) {
 	}
 
 	return result, nil
+}
+
+func (s *Store) filter(match func(Meta) bool) []Meta {
+	result := make([]Meta, 0)
+	for _, app := range s.apps {
+		if match(app) {
+			result = append(result, app)
+		}
+	}
+
+	return result
+}
+
+func (s *Store) firstOrNil(match func(Meta) bool) *Meta {
+	for _, app := range s.apps {
+		if match(app) {
+			return &app
+		}
+	}
+
+	return nil
+}
+
+func (s *Store) lastOrNil(match func(Meta) bool) *Meta {
+	for i := len(s.apps) - 1; i >= 0; i-- {
+		app := s.apps[i]
+		if match(app) {
+			return &app
+		}
+	}
+
+	return nil
 }
--- src/server.go
+++ src/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -19,6 +20,16 @@ func newApp(c *gin.Context) {
 	}
 	store.Add(app)
 	c.JSON(http.StatusCreated, app)
+}
+
+func getAppByTitle(c *gin.Context) {
+	title := c.Param("title")
+	app := store.GetByTitle(title)
+	if app != nil {
+		c.JSON(http.StatusOK, app)
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("App with title '%s' does not exist.", title)})
+	}
 }
 
 func listApps(c *gin.Context) {
@@ -41,6 +52,7 @@ func main() {
 	{
 		v1.POST("/apps", newApp)
 		v1.GET("/apps", listApps)
+		v1.GET("/apps/:title", getAppByTitle)
 	}
 
 	router.Run(":3001")
