7c36bebe6162db0099321c18fc3d1b767ba5b82f
Zhidong Zhu <zhizhu@microsoft.com>
2022-01-12 22:31:55 +0800

    add store_test

---
+++ src/app/store_test.go
@@ -0,0 +1,165 @@
+package app
+
+import (
+	"testing"
+
+	"github.com/zzn2/demo/appstore/filter"
+)
+
+var (
+	app1v1 = Meta{
+		Title:   "App1",
+		Version: "0.0.1",
+	}
+
+	app1v2 = Meta{
+		Title:   "App1",
+		Version: "0.0.2",
+	}
+
+	app2v1 = Meta{
+		Title:   "App2",
+		Version: "0.0.1",
+	}
+)
+
+func TestAdd(t *testing.T) {
+	var store Store
+
+	if len(store.apps) != 0 {
+		t.Errorf("Expected store is empty but contained %d apps.", len(store.apps))
+	}
+
+	err := store.Add(app1v1)
+
+	if len(store.apps) != 1 {
+		t.Errorf("Expected store contains 1 app but actually contained %d apps.", len(store.apps))
+	}
+	if err != nil {
+		t.Errorf("Should not have error but error '%s' occurred.", err.Error())
+	}
+
+	err = store.Add(app1v2)
+
+	if len(store.apps) != 2 {
+		t.Errorf("Expected store contains 2 apps but actually contained %d apps.", len(store.apps))
+	}
+	if err != nil {
+		t.Errorf("Should not have error but error '%s' occurred.", err.Error())
+	}
+
+	err = store.Add(app1v2)
+
+	if len(store.apps) != 2 {
+		t.Errorf("Expected store contains 2 apps but actually contained %d apps.", len(store.apps))
+	}
+	if err == nil {
+		expectedErrMsg := "App 'App1' with version '0.0.2' already exists."
+		if err.Error() != expectedErrMsg {
+			t.Errorf("Expected error message to be '%s' but got '%s'.", expectedErrMsg, err.Error())
+		}
+	}
+}
+
+func equals(app1 Meta, app2 Meta) bool {
+	return app1.Title == app2.Title && app1.Version == app2.Version
+}
+
+func TestGetByTitle(t *testing.T) {
+	var store Store
+
+	store.Add(app1v1)
+	store.Add(app1v2)
+	store.Add(app2v1)
+
+	result1 := store.GetByTitle("App1")
+	if !equals(*result1, app1v2) {
+		t.Errorf("Expected to be '%s' but got '%s'", result1, app1v2)
+	}
+
+	result2 := store.GetByTitle("App2")
+	if !equals(*result2, app2v1) {
+		t.Errorf("Expected to be '%s' but got '%s'", result2, app2v1)
+	}
+
+	result3 := store.GetByTitle("App3")
+	if result3 != nil {
+		t.Errorf("Expected to be nil but got '%s'", result3)
+	}
+}
+
+func TestGetByTitleAndVersion(t *testing.T) {
+	var store Store
+
+	store.Add(app1v1)
+	store.Add(app1v2)
+	store.Add(app2v1)
+
+	result1 := store.GetByTitleAndVersion("App1", "0.0.1")
+	if !equals(*result1, app1v1) {
+		t.Errorf("Expected to be '%s' but got '%s'", result1, app1v2)
+	}
+
+	result2 := store.GetByTitleAndVersion("App1", "0.0.2")
+	if !equals(*result2, app1v2) {
+		t.Errorf("Expected to be '%s' but got '%s'", result2, app1v2)
+	}
+
+	result3 := store.GetByTitleAndVersion("App1", "0.0.3")
+	if result3 != nil {
+		t.Errorf("Expected to be nil but got '%s'", result3)
+	}
+
+	result4 := store.GetByTitleAndVersion("App2", "0.0.1")
+	if !equals(*result4, app2v1) {
+		t.Errorf("Expected to be '%s' but got '%s'", result4, app2v1)
+	}
+
+	result5 := store.GetByTitleAndVersion("App3", "0.0.1")
+	if result5 != nil {
+		t.Errorf("Expected to be nil but got '%s'", result5)
+	}
+}
+
+func TestList(t *testing.T) {
+	var store Store
+
+	store.Add(app1v1)
+	store.Add(app1v2)
+	store.Add(app2v1)
+
+	var ruleSet filter.RuleSet
+
+	result, err := store.List(ruleSet)
+	if err != nil {
+		t.Errorf("Expected to be no error but got '%s'", err.Error())
+	}
+	if len(result) != 3 {
+		t.Errorf("Expected to be 3 items but got %d", len(result))
+	}
+
+	rule, _ := filter.ParseRule("title=App1")
+	ruleSet.AddRule(*rule)
+
+	result, err = store.List(ruleSet)
+	if err != nil {
+		t.Errorf("Expected to be no error but got '%s'", err.Error())
+	}
+	if len(result) != 2 {
+		t.Errorf("Expected to be 2 items but got %d", len(result))
+	}
+
+	rule, _ = filter.ParseRule("unknown=App1")
+	ruleSet.AddRule(*rule)
+
+	result, err = store.List(ruleSet)
+	if err != nil {
+		expectedErrMsg := "Error occurred during searching app: Unsupported rule for field 'unknown'"
+		if err.Error() != expectedErrMsg {
+			t.Errorf("Expected error message to be '%s' but got '%s'", expectedErrMsg, err.Error())
+		}
+	}
+	if len(result) != 0 {
+		t.Errorf("Expected to be 0 items but got %d", len(result))
+	}
+}
