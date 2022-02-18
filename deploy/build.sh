687a0a6e160d4ed82b803273a1887f57e5949d20
Zhidong Zhu <zhizhu@microsoft.com>
2022-02-18 21:28:41 +0800

    deploy using helm

---
+++ deploy/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Run in the root folder in this repo, invoke like:
+# ./deploy/build.sh
+
+REGISTRY=zzn2
+COMMIT=$(git rev-parse HEAD | cut -c 1-8)
+
+# Build docker image
+IMAGE_NAME=$REGISTRY/app-store-demo:$COMMIT
+docker build . -t $IMAGE_NAME
+docker push $IMAGE_NAME
+
+# Deploy with Helm
+helm upgrade --install app-store ./deploy/app-store/ --set image.registry=$REGISTRY,image.tag=$COMMIT
