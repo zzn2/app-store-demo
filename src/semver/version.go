072837de7226528457dd5ba8bac1b7fb3a92449a
Zhidong Zhu <zhizhu@microsoft.com>
2022-03-01 21:06:24 +0800

    make semver.Version comparable

--- src/semver/version.go
+++ src/semver/version.go
@@ -12,39 +12,16 @@ import (
 	"strings"
 )
 
+// Version represents a Semantic Version.
+// However here only implements a subset of the full spec for demo usage.
 type Version struct {
 	Major uint64
 	Minor uint64
 	Patch uint64
 }
 
+// Empty represents the zero value of Version.
 var Empty = Version{}
-
-func containsOnly(s string, set string) bool {
-	return strings.IndexFunc(s, func(r rune) bool {
-		return !strings.ContainsRune(set, r)
-	}) == -1
-}
-
-func hasLeadingZeroes(s string) bool {
-	return len(s) > 1 && s[0] == '0'
-}
-
-func parseSection(text string) (uint64, error) {
-	if !containsOnly(text, "0123456789") {
-		return 0, fmt.Errorf("Invalid character(s) found in number %q", text)
-	}
-	if hasLeadingZeroes(text) {
-		return 0, fmt.Errorf("Version sections must not contain leading zeroes: %q", text)
-	}
-
-	res, err := strconv.ParseUint(text, 10, 64)
-	if err != nil {
-		return 0, err
-	}
-
-	return res, nil
-}
 
 // Parse parses a text into a Version object.
 // errors will be returned if the text is not a valid format.
@@ -76,6 +53,16 @@ func Parse(s string) (Version, error) {
 	return v, nil
 }
 
+// LessThan checks whether the current version is less than another given version.
+func (v Version) LessThan(another interface{}) bool {
+	return v.compareTo(another) < 0
+}
+
+// GreaterThan checks whether the current version is greater than another given version.
+func (v Version) GreaterThan(another interface{}) bool {
+	return v.compareTo(another) > 0
+}
+
 // UnmarshalYAML will be called when deserializing a Version object from part of YAML text.
 func (v *Version) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	var text string
@@ -84,7 +71,12 @@ func (v *Version) UnmarshalYAML(unmarshal func(interface{}) error) error {
 		return err
 	}
 
-	version, err := Parse(text)
+	return v.UnmarshalText([]byte(text))
+}
+
+// UnmarshalText reads list of bytes and parse them into Version object.
+func (v *Version) UnmarshalText(text []byte) error {
+	version, err := Parse(string(text))
 	if err != nil {
 		return err
 	}
@@ -103,4 +95,56 @@ func (v Version) MarshalJSON() ([]byte, error) {
 // String returns the string representation of the Version object.
 func (v Version) String() string {
 	return fmt.Sprintf("%d.%d.%d", v.Major, v.Minor, v.Patch)
+}
+
+func (v Version) compareTo(another interface{}) int {
+	if _, ok := another.(Version); !ok {
+		panic("Expected Version type.")
+	}
+
+	if v.Major < another.(Version).Major {
+		return -1
+	} else if v.Major > another.(Version).Major {
+		return 1
+	} else {
+		if v.Minor < another.(Version).Minor {
+			return -1
+		} else if v.Minor > another.(Version).Minor {
+			return 1
+		} else {
+			if v.Patch < another.(Version).Patch {
+				return -1
+			} else if v.Patch > another.(Version).Patch {
+				return 1
+			} else {
+				return 0
+			}
+		}
+	}
+}
+
+func containsOnly(s string, set string) bool {
+	return strings.IndexFunc(s, func(r rune) bool {
+		return !strings.ContainsRune(set, r)
+	}) == -1
+}
+
+func hasLeadingZeroes(s string) bool {
+	return len(s) > 1 && s[0] == '0'
+}
+
+func parseSection(text string) (uint64, error) {
+	if !containsOnly(text, "0123456789") {
+		return 0, fmt.Errorf("Invalid character(s) found in number %q", text)
+	}
+	if hasLeadingZeroes(text) {
+		return 0, fmt.Errorf("Version sections must not contain leading zeroes: %q", text)
+	}
+
+	res, err := strconv.ParseUint(text, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return res, nil
 }
