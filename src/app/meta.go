654eff8758bdcfce00aaae0c987c0345a811fa3a
Zhidong Zhu <zhizhu@microsoft.com>
2022-03-01 21:29:42 +0800

    remove methods from Meta

--- src/app/meta.go
+++ src/app/meta.go
@@ -1,13 +1,9 @@
 package app
 
 import (
-	"errors"
 	"fmt"
-	"strings"
 
-	"github.com/zzn2/demo/appstore/filter"
 	"github.com/zzn2/demo/appstore/semver"
-	"gopkg.in/yaml.v2"
 )
 
 type Maintainer struct {
@@ -24,68 +20,6 @@ type Meta struct {
 	Source      string         `binding:"required"`
 	License     string         `binding:"required"`
 	Description string         `binding:"required"`
-}
-
-// Parse parses an yaml text into the Meta struct.
-// It returns nil along with the error if any error occurred during the parse.
-func Parse(data []byte) (*Meta, error) {
-	var m Meta
-	if err := yaml.Unmarshal(data, &m); err != nil {
-		return nil, err
-	}
-
-	return &m, nil
-}
-
-// MatchRule evaluates whether this Meta matches the given rule.
-// It returns true if matches otherwise returns false.
-// If any unexpected errors occurred during match operation, return the error.
-func (m *Meta) MatchRule(rule filter.Rule) (bool, error) {
-	// TODO: embed the logic into the struct using struct tags
-	// https://www.digitalocean.com/community/tutorials/how-to-use-struct-tags-in-go
-	switch strings.ToLower(rule.FieldName) {
-	case "title":
-		return rule.Evaluate(m.Title)
-	case "version":
-		return rule.Evaluate(m.Version.String())
-	case "maintainer.name":
-		for _, maintainer := range m.Maintainers {
-			match, err := rule.Evaluate(maintainer.Name)
-			if err != nil {
-				return false, err
-			}
-			if match {
-				// Early return when the first match found.
-				// If not found, do not return and go to next loop.
-				return true, nil
-			}
-		}
-		// All maintainers not match if goes to this line.
-		return false, nil
-	case "company":
-		return rule.Evaluate(m.Company)
-	case "description":
-		return rule.Evaluate(m.Description)
-	default:
-		return false, errors.New(fmt.Sprintf("Unsupported rule for field '%s'", rule.FieldName))
-	}
-}
-
-// MatchRuleSet evaluates whether this Meta matches the given ruleset.
-// It returns true if matches otherwise returns false.
-// If any unexpected errors occurred during match operation, return the error.
-func (m *Meta) MatchRuleSet(ruleSet filter.RuleSet) (bool, error) {
-	for _, rule := range ruleSet.Rules {
-		match, err := m.MatchRule(rule)
-		if err != nil {
-			return false, err
-		}
-		if !match {
-			return false, nil
-		}
-	}
-
-	return true, nil
 }
 
 // String returns the string representation of this object.
--- src/app/meta_test.go
+++ src/app/meta_test.go
@@ -3,91 +3,34 @@ package app
 import (
 	"testing"
 
-	"github.com/zzn2/demo/appstore/filter"
+	"github.com/zzn2/demo/appstore/semver"
 )
 
-const sampleInput = `
-title: Valid App 1
-version: 0.0.1
-maintainers:
-- name: firstmaintainer app1
-  email: firstmaintainer@hotmail.com
-- name: secondmaintainer app1
-  email: secondmaintainer@gmail.com
-company: Random Inc.
-website: https://website.com
-source: https://github.com/random/repo
-license: Apache-2.0
-description: |
- ### Interesting Title
+var app = Meta{
+	Title:   "Valid App 1",
+	Version: semver.Version{Major: 0, Minor: 0, Patch: 1},
+	Maintainers: []Maintainer{
+		{
+			Name:  "Alice",
+			Email: "alice@hotmail.com",
+		},
+		{
+			Name:  "Bob",
+			Email: "bob@gmail.com",
+		},
+	},
+	Company: "Random Inc.",
+	Website: "https://website.com",
+	Source:  "https://github.com/random/repo",
+	License: "Apache-2.0",
+	Description: `
+ ### Intresting Title
  Some application content, and description
-`
-
-func TestParse(t *testing.T) {
-	app, err := Parse([]byte(sampleInput))
-
-	if err != nil {
-		t.Fatalf(`Expected to be succeed but failed`)
-	}
-	if app.Title != "Valid App 1" {
-		t.Fatalf("failed")
-	}
+`,
 }
 
-func TestMatchRule(t *testing.T) {
-	var tests = []struct {
-		ruleText       string
-		expectedResult bool
-		expectedErrMsg string
-	}{
-		{
-			"title[like]=App",
-			true,
-			"",
-		},
-		{
-			"title=App",
-			false,
-			"",
-		},
-		{
-			"title=Valid App 1",
-			true,
-			"",
-		},
-		{
-			"maintainer.name[like]=second",
-			true,
-			"",
-		},
-		{
-			"maintainer.name[like]=third",
-			false,
-			"",
-		},
-	}
-
-	app, err := Parse([]byte(sampleInput))
-	if err != nil {
-		t.Fatalf("Failed to parse app meta.")
-	}
-
-	for _, tt := range tests {
-		testName := tt.ruleText
-		t.Run(testName, func(t *testing.T) {
-			rule, err := filter.ParseRule(tt.ruleText)
-			if err != nil {
-				t.Fatalf("Failed to parse rule %s", tt.ruleText)
-			}
-			match, err := app.MatchRule(*rule)
-			if match != tt.expectedResult {
-				t.Errorf("Expect '%v' but got '%v'.", tt.expectedResult, match)
-			}
-			if err != nil {
-				if err.Error() != tt.expectedErrMsg {
-					t.Errorf("Expect err to be '%s' but got '%s'.", tt.expectedErrMsg, err.Error())
-				}
-			}
-		})
+func TestString(t *testing.T) {
+	if app.String() != "App: Valid App 1@0.0.1" {
+		t.Errorf("Failed")
 	}
 }
